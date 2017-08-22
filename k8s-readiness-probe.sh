30021b7c3de16f8a56f037220bb3007ceb019ae6
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-08-22 12:56:52 +0200

    Update k8s-readiness-probe.sh

--- k8s-readiness-probe.sh
+++ k8s-readiness-probe.sh
@@ -3,3 +3,20 @@
 set -e
 
 curl -s --insecure https://localhost:8443 > /dev/null
+
+##
+## USAGE inside a stateful set of kubernetes
+##
+#  livenessProbe:
+#    exec:
+#      command: 
+#      - bash
+#      - /work-private/k8s-readiness-probe.sh
+#    initialDelaySeconds: 60
+#    timeoutSeconds: 5
+#  readinessProbe:
+#      command: 
+#      - bash
+#      - /work-private/k8s-readiness-probe.sh
+#    initialDelaySeconds: 60
+#    timeoutSeconds: 5
